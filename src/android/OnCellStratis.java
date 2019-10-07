5cf9f5ec0b6803af529146fc4bea53cdd7e6d951
Keenan Bartlett <keenancbartlett@gmail.com>
2019-10-07 15:55:38 -0500

    Implement Android setAccessToken and setServerEnvironment

--- src/android/OnCellStratis.java
+++ src/android/OnCellStratis.java
@@ -13,22 +13,40 @@ import org.json.JSONException;
 
 import android.util.Log;
 
-// import com.stratisiot.stratissdk.StratisSDK
-// import com.stratisiot.stratissdk.ServerEnvironment
+import com.stratisiot.stratissdk.StratisSDK;
+import com.stratisiot.stratissdk.StratisSDK.ResultCallback;
+import com.toursphere.oncellstratis.MainActivity;
 
 public class OnCellStratis extends CordovaPlugin {
     private static final String TAG = "OnCellStratis";
+    private StratisSDK stratisSDK;
     private String accessToken;
-  
+    private String serverEnvironmentString;
+
     public OnCellStratis() {
         super();
         this.accessToken = "";
+        this.serverEnvironmentString = "";
+    }
+
+    class InitCallback implements ResultCallback {
+        public void error(JSONObject error) {
+            try { Log.e(TAG, "Error on init: " + error.getString("message")); } catch (JSONException e) { e.printStackTrace(); }
+        }
+        public void result(JSONObject r) {
+            try { Log.d(TAG, "Init result: " + r.getString("message")); } catch (JSONException e) { e.printStackTrace(); }
+        }
+        public void done() { Log.d(TAG, "Init done"); }
+        public void done(JSONObject r) { Log.d(TAG, "Init done with result JSON"); }
     }
   
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         Log.d(TAG, "OnCellStratis plugin initialize");
         super.initialize(cordova, webView);
+        MainActivity mainActivity = (MainActivity) cordova.getActivity();
+        InitCallback initCallback = new InitCallback();
+        stratisSDK = new StratisSDK(mainActivity, initCallback);
     }
     
     private enum Commands {
@@ -40,8 +58,8 @@ public class OnCellStratis extends CordovaPlugin {
         Commands cmd = Commands.valueOf(action);
         switch (cmd) {
             case setServerEnvironment:
-                String serverEnvironment = args.getString(0);
-                this.setServerEnvironment(serverEnvironment, callbackContext);
+                String serverEnvironmentString = args.getString(0);
+                this.setServerEnvironment(serverEnvironmentString, callbackContext);
                 return true;
             case setAccessToken:
                 String accessToken = args.getString(0);
@@ -63,10 +81,29 @@ public class OnCellStratis extends CordovaPlugin {
                 return false;
         }
     }
+
+    /* Begin command functions */
     
-    public void setServerEnvironment(String serverEnvironment, CallbackContext callbackContext) {
-        if (serverEnvironment != null && serverEnvironment.length() > 0) {
-            this._setServerEnvironment(serverEnvironment);
+    public void setServerEnvironment(String serverEnvironmentString, CallbackContext callbackContext) {
+
+        if (isValidServerEnvironment(serverEnvironmentString)) {
+            Log.d(TAG, "setServerEnvironment " + serverEnvironmentString);
+            this.serverEnvironmentString = serverEnvironmentString;
+
+            class ServerEnvironmentCallback implements ResultCallback {
+                public void error(JSONObject error) {
+                    try { Log.e(TAG, "Error setting server environment: " + error.getString("message")); } catch (JSONException e) { e.printStackTrace(); }
+                }
+                public void result(JSONObject r) {
+                    try { Log.d(TAG, "Server environment set: " + r.getString("message")); } catch (JSONException e) { e.printStackTrace(); }
+                }
+                public void done() { Log.d(TAG, "Server environment done"); }
+                public void done(JSONObject r) { Log.d(TAG, "Server environment done with result JSON"); }
+            }
+
+            ServerEnvironmentCallback serverEnvironmentCallback = new ServerEnvironmentCallback();
+            stratisSDK.setServerEnvironment(StratisSDK.ServerEnvironment.valueOf(serverEnvironmentString), serverEnvironmentCallback);
+
             JSONObject r = new JSONObject();
             try {
                 r.put("success", 1);
@@ -75,13 +112,30 @@ public class OnCellStratis extends CordovaPlugin {
             }
             callbackContext.success(r.toString());
         } else {
-            callbackContext.error("Expected a server environment");
+            callbackContext.error("Expected a valid server environment");
         }
     }
-    
+
     public void setAccessToken(String accessToken, CallbackContext callbackContext) {
+
         if (accessToken != null && accessToken.length() > 0) {
-            this._setAccessToken(accessToken);
+            Log.d(TAG, "setAccessToken " + accessToken);
+            this.accessToken = accessToken;
+
+            class AccessTokenCallback implements ResultCallback {
+                public void error(JSONObject error) {
+                    try { Log.e(TAG, "Error setting access token: " + error.getString("message")); } catch (JSONException e) { e.printStackTrace(); }
+                }
+                public void result(JSONObject r) {
+                    try { Log.d(TAG, "Access token set: " + r.getString("message")); } catch (JSONException e) { e.printStackTrace(); }
+                }
+                public void done() { Log.d(TAG, "Access token done"); }
+                public void done(JSONObject r) { Log.d(TAG, "Access token done with result JSON"); }
+            }
+
+            AccessTokenCallback accessTokenCallback = new AccessTokenCallback();
+            stratisSDK.setAccessToken(accessToken, accessTokenCallback);
+
             JSONObject r = new JSONObject();
             try {
                 r.put("success", 1);
@@ -139,26 +193,40 @@ public class OnCellStratis extends CordovaPlugin {
         }
     }
     
-    public void _setServerEnvironment(String serverEnvironmentString) {
-        // create ServerEnvironment object from string
-        // StratisSDK.setServerEnvironment(serverEnvironment, setServerEnvironmentCallback);
-    }
-    
-    public void _setAccessToken(String accessToken) {
-        Log.d(TAG, "setAccessToken " + accessToken);
-        this.accessToken = accessToken;
-        // StratisSDK.setAccessToken(accessToken, setAccessTokenCallback);
-    }
-    
     public void _getLocks(String property) {
+        Log.d(TAG, "_getLocks " + property);
         // StratisSDK.getLocks(property, getLocksCallback);
     }
     
     public void _scanLocks(Long seconds) {
+        Log.d(TAG, "_scanLocks: " + seconds);
         // StratisSDK.scanLocks(seconds, scanLocksCallback);
     }
     
     public void _activateLock(String lockId) {
+        Log.d(TAG, "_activateLock " + lockId);
         // StratisSDK.activateLock(lockId, activateLockCallback);
     }
+
+    /* End command functions */
+
+    /* Begin utility functions */
+
+    public boolean isValidServerEnvironment(String serverEnvironmentString) {
+        // Valid serverEnvironmentStrings:
+        // DEV, SANDBOX, TEST, PROD
+        // see https://developer.stratisiot.com/sdk/android/com.stratisiot.stratissdk/-server-environment/ for more info
+        try {
+            StratisSDK.ServerEnvironment.valueOf(serverEnvironmentString);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    // NOTE do we need functions to get and set status of bluetooth?
+
+    /* End utility functions*/
+
+
 }
