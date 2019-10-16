b184feec6a7b3e1994c0623663b8b8880b652a6d
Keenan Bartlett <keenancbartlett@gmail.com>
2019-10-16 10:30:36 -0500

    Fix iOS native code indentation and clean up

--- src/ios/OnCellStratis.swift
+++ src/ios/OnCellStratis.swift
@@ -2,88 +2,74 @@ import StratisSDK
 
 @objc(OnCellStratis) class OnCellStratis : CDVPlugin {
     
-  @objc(setAccessToken:)
-  func setAccessToken(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-
-    let msg = command.arguments[0] as? String ?? ""
-
-    if msg.characters.count > 0 {
-      let toastController: UIAlertController =
-        UIAlertController(
-          title: "",
-          message: msg,
-          preferredStyle: .alert
-        )
-      
-      self.viewController?.present(
-        toastController,
-        animated: true,
-        completion: nil
-      )
-
-      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-        toastController.dismiss(
-          animated: true,
-          completion: nil
-        )
-      }
+    @objc(pluginInitialize)
+    override func pluginInitialize() {
         
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: msg
-      )
     }
-
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
-  
-  @objc(setServerEnvironment:)
-  func setServerEnvironment(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
-  
-  @objc(getLocks:)
-  func getLocks(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
-  
-  @objc(scanLocks:)
-  func scanLocks(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
-  
-  @objc(activateLock:)
-  func activateLock(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
+    
+    @objc(setAccessToken:)
+    func setAccessToken(command: CDVInvokedUrlCommand) {
+        
+        //    let arg = command.arguments[0] as? String ?? ""
+        
+        //    var pluginResult = CDVPluginResult(
+        //      status: CDVCommandStatus_ERROR
+        //    )
+        
+        let message = "{\"success\": 1, \"accessToken\": \"test-access-token\"}"
+        
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: message
+        )
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    
+    @objc(setServerEnvironment:)
+    func setServerEnvironment(command: CDVInvokedUrlCommand) {
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    
+    @objc(getLocks:)
+    func getLocks(command: CDVInvokedUrlCommand) {
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    
+    @objc(scanLocks:)
+    func scanLocks(command: CDVInvokedUrlCommand) {
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    
+    @objc(activateLock:)
+    func activateLock(command: CDVInvokedUrlCommand) {
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
 }
